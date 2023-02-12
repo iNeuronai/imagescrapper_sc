048b15bb96f71c8089aea7c4846c1ba3381df2ab
iNeuronai <sudhanshu@ineuron.ai>
2023-02-12 16:36:33 +0530

    first commit

--- scraper.py
+++ scraper.py
@@ -101,7 +101,7 @@ def search_and_download(search_term: str, driver_path: str, target_path='./image
 
 
 DRIVER_PATH = r'chromedriver.exe'
-search_term = 'sudhanshu kumar'
+search_term = 'trump'
 # num of images you can pass it from here  by default it's 10 if you are not passing
 #number_images = 50
 search_and_download(search_term=search_term, driver_path=DRIVER_PATH, number_images=50)
